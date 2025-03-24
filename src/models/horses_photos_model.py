eb62ac31483ecd29213210ebe47006ee6f291f60
Antony Pavlenko <antonpawlenko@yandex.ru>
2025-03-24 16:32:56 +0300

    added some resolvind tables and models for minio

---
+++ src/models/horses_photos_model.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from sqlalchemy import String, Integer, func, ForeignKey, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base_model import Base
+
+
+class HorsesPhotosModel(Base):
+    __tablename__ = "horses_photos"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True) #Unique id
+    organization_id: Mapped[int] = mapped_column(ForeignKey("horses.id")) #Bucket name
+    file_id: Mapped[int] = mapped_column(ForeignKey("media_files.id")) #Bucket name
