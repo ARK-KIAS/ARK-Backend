ad04277fe67d790e29eea61820feece14551cc4e
nanoiridium <islava858@gmail.com>
2025-05-27 20:47:25 +0300

    added GET for race day

--- src/races_days_routes.py
+++ src/races_days_routes.py
@@ -6,6 +6,8 @@ from src.schemas.race_days_schema import RaceDaysCreate, RaceDaysUpdate, RaceDay
 from src.repositories.race_days_repository import race_days_repository
 
 from .misc_functions import is_authorized
+from .repositories.races_race_days_repository import races_race_days_repository
+from .repositories.races_repository import races_repository
 
 race_days_router = APIRouter(prefix="/races_days", tags=["race_days"])
 
@@ -26,7 +28,23 @@ async def get_orgs():
 async def get_orgs(id: int):
     race_days = await race_days_repository.get_single(id=id)
 
-    return JSONResponse(content={'race_days': jsonable_encoder(race_days)}, status_code=200)
+    if race_days is None:
+        return JSONResponse(content={'message': 'There is no race day with that ID!'}, status_code=404)
+
+    races_race_days = await races_race_days_repository.get_multi_filtered(race_days_id=race_days.id)
+
+    if len(races_race_days) == 0:
+        return JSONResponse(content={'message': 'There is no races on that day!'}, status_code=404)
+
+    races = []
+    for race in races_race_days:
+        race_dict = (await races_repository.get_single(id=race.race_id)).__dict__
+        races.append(race_dict)
+
+    race_day_dict = race_days.__dict__
+    race_day_dict["races"] = races
+
+    return JSONResponse(content={'race_day': jsonable_encoder(race_day_dict)}, status_code=200)
 
 
 @race_days_router.put('/{id}', dependencies=[Depends(is_authorized)], response_model=RaceDaysResponse)
